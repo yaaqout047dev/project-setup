c8da0f03601bd1e4ca4916e984811c05a2b3d8e7
yaaqout <27elruby@gmail.com>
2025-07-20 23:05:44 +0300

    linking to MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+
 
 app.get('/', (req, res) => {
   res.sendFile('./views/home.html', {root: __dirname})
 })
 
-app.listen(port, () => {
+mongoose.connect('mongodb+srv://yaaqout8tareq:nkenegmCovedDmW5@cluster0.o8d2q.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+  app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
 })
+
+}
+).catch((err) => {
+  console.log(err);
+})
+
+
+
+
+
+
